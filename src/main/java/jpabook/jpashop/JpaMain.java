127713af7c701d42ccd87cd697b3e87f40725790
Choising <chltmdals115@gmail.com>
2021-12-17 21:05:12 +0900

    단방향 연관관계 - 2

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -19,14 +19,20 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // save
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            // select
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
             tx.commit();
         } catch (Exception e) {
